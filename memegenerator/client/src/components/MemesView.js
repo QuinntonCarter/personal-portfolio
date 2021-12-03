44a0db0f56026487ff3b482d8ed14d81440debf4
QuinntonCarter <qdcarter92@protonmail.com>
2021-12-03 16:51:15 -0700

    fixed some css

--- memegenerator/client/src/components/MemesView.js
+++ memegenerator/client/src/components/MemesView.js
@@ -6,7 +6,7 @@ export default function MemesView(props){
 
     
     const mappedMemes = memes ? memes.map(meme => 
-        <div className='p-4 px-5' key={meme._id}>
+        <div className='p-4 px-5 mx-auto' key={meme._id}>
             <h5 className='p-1 bg-blue-400 text-white text-xs'> Created {meme.created.slice(0,10)} by '{meme.alias || meme._id.slice(14)}'</h5>
             <img className='border-blue-400 border-solid border-4' src={meme.imgSrc} alt={`user meme: ${meme._id}`} />
         </div>
